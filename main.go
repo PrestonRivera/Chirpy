d77eeb00bb69c4fd63871d93a072ba14f82a89bb
PrestonRivera <prestonrivera211@gmail.com>
2025-01-20 02:13:25 -0600

    Completed Chapter 1: HTTP handlers, fileServers, work flow

--- main.go
+++ main.go
@@ -1,6 +1,36 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
 
-func helloWorld() {
-	fmt.Println("Hello World")
+// run your server:   go build -o out && ./out
+
+
+func main() {
+	const filePathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+
+	s := &http.Server{
+		Addr: ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filePathRoot, port)
+	err := s.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Server failed: %v", err.Error())
+	}
+}
+
+
+func handlerReadiness(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
